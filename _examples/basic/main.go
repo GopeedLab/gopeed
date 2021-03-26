d3c5dafd3edb74c0d47f534491e96d68a6805c01
liwei <liwei2633@163.com>
2021-03-26 16:23:24 +0800

    ci: auto release

---
+++ _examples/basic/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/monkeyWie/gopeed-core/pkg/base"
+	"github.com/monkeyWie/gopeed-core/pkg/download"
+)
+
+func main() {
+	finallyCh := make(chan error)
+	err := download.Boot().
+		URL("https://www.baidu.com/index.html").
+		Listener(func(event *download.Event) {
+			if event.Key == download.EventKeyFinally {
+				finallyCh <- event.Err
+			}
+		}).
+		Create(&base.Options{
+			Connections: 8,
+		})
+	if err != nil {
+		panic(err)
+	}
+	err = <-finallyCh
+	if err != nil {
+		fmt.Printf("download fail:%v\n", err)
+	} else {
+		fmt.Println("download success")
+	}
+}
