1b01b09cc2e5d820fad86c0a796db862e6c1ad21
liwei <liwei-8466@qq.com>
2022-11-13 23:14:33 +0800

    temp

--- ui/flutter/lib/pages/setting/setting_controller.dart
+++ ui/flutter/lib/pages/setting/setting_controller.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
 
 class SettingController extends GetxController {
-  final tapStatues = <int, bool>{}.obs;
+  final basicTapStatues = <int, bool>{}.obs;
+  final advancedTapStatues = <int, bool>{}.obs;
 
   void clearTapStatus() {
-    tapStatues.clear();
+    basicTapStatues.clear();
+    advancedTapStatues.clear();
   }
 }
--- ui/flutter/lib/pages/setting/setting_view.dart
+++ ui/flutter/lib/pages/setting/setting_view.dart
@@ -20,7 +20,8 @@ class SettingView extends GetView<SettingController> {
   @override
   Widget build(BuildContext context) {
     final appController = Get.find<AppController>();
-    final setting = appController.downloaderConfig.value;
+    final downloaderCfg = appController.downloaderConfig.value;
+    final startCfg = appController.startConfig.value;
 
     Timer? timer;
     debounceSave() {
@@ -35,156 +36,231 @@ class SettingView extends GetView<SettingController> {
       return completer.future;
     }
 
-    return Scaffold(
-      body: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: PreferredSize(
-              preferredSize: const Size.fromHeight(56),
-              child: AppBar(
-                bottom: TabBar(
-                  tabs: [
-                    Tab(
-                      text: "setting.basic".tr,
-                    ),
-                    Tab(
-                      text: "setting.advanced".tr,
-                    ),
-                  ],
-                ),
+    final basicConfigItems = [
+      _ConfigItem(
+          'setting.theme'.tr,
+          () => _getThemeName(downloaderCfg.extra!.themeMode),
+          (Key key) => DropdownButton<String>(
+                key: key,
+                value: downloaderCfg.extra!.themeMode,
+                isDense: true,
+                onChanged: (value) async {
+                  downloaderCfg.extra!.themeMode = value!;
+                  controller.clearTapStatus();
+                  Get.changeThemeMode(ThemeMode.values.byName(value));
+
+                  await debounceSave();
+                },
+                items: ThemeMode.values
+                    .map((e) => DropdownMenuItem<String>(
+                          value: e.name,
+                          child: Text(_getThemeName(e.name)),
+                        ))
+                    .toList(),
               )),
-          body: TabBarView(
-            children: [
-              Obx(() => Card(
-                      child: Column(
-                    children: _buildConfigItems([
-                      _ConfigItem(
-                          'setting.theme'.tr,
-                          () => _getThemeName(setting.extra!.themeMode),
-                          () => DropdownButton<String>(
-                                value: setting.extra!.themeMode,
-                                isDense: true,
-                                onChanged: (value) async {
-                                  setting.extra!.themeMode = value!;
-                                  controller.clearTapStatus();
-                                  Get.changeThemeMode(
-                                      ThemeMode.values.byName(value));
+      _ConfigItem('setting.downloadDir'.tr, () => downloaderCfg.downloadDir,
+          (Key key) {
+        final downloadDirController =
+            TextEditingController(text: downloaderCfg.downloadDir);
+        downloadDirController.addListener(() async {
+          if (downloadDirController.text != downloaderCfg.downloadDir) {
+            downloaderCfg.downloadDir = downloadDirController.text;
+            if (Util.isDesktop()) {
+              controller.clearTapStatus();
+            }
 
-                                  await debounceSave();
-                                },
-                                items: ThemeMode.values
-                                    .map((e) => DropdownMenuItem<String>(
-                                          value: e.name,
-                                          child: Text(_getThemeName(e.name)),
-                                        ))
-                                    .toList(),
-                              )),
-                      _ConfigItem(
-                          'setting.downloadDir'.tr, () => setting.downloadDir,
-                          () {
-                        final downloadDirController =
-                            TextEditingController(text: setting.downloadDir);
-                        downloadDirController.addListener(() async {
-                          if (downloadDirController.text !=
-                              setting.downloadDir) {
-                            setting.downloadDir = downloadDirController.text;
-                            if (Util.isDesktop()) {
-                              controller.clearTapStatus();
-                            }
+            await debounceSave();
+          }
+        });
+        return DirectorySelector(
+          controller: downloadDirController,
+          showLabel: false,
+        );
+      }),
+      // _ConfigItem('setting.connections'.tr,
+      //     () => controller.setting.value.connections.toString(),
+      //     () {
+      //   final connectionsController = TextEditingController(
+      //       text: controller.setting.value.connections.toString());
+      //   connectionsController.addListener(() async {
+      //     if (connectionsController.text.isNotEmpty &&
+      //         connectionsController.text !=
+      //             controller.setting.value.connections.toString()) {
+      //       controller.setting.value.connections =
+      //           int.parse(connectionsController.text);
 
-                            await debounceSave();
-                          }
-                        });
-                        return DirectorySelector(
-                          controller: downloadDirController,
-                          showLabel: false,
-                        );
-                      }),
-                      // _ConfigItem('setting.connections'.tr,
-                      //     () => controller.setting.value.connections.toString(),
-                      //     () {
-                      //   final connectionsController = TextEditingController(
-                      //       text: controller.setting.value.connections.toString());
-                      //   connectionsController.addListener(() async {
-                      //     if (connectionsController.text.isNotEmpty &&
-                      //         connectionsController.text !=
-                      //             controller.setting.value.connections.toString()) {
-                      //       controller.setting.value.connections =
-                      //           int.parse(connectionsController.text);
+      //       await debounceSave();
+      //     }
+      //   });
 
-                      //       await debounceSave();
-                      //     }
-                      //   });
+      //   return TextField(
+      //     controller: connectionsController,
+      //     keyboardType: TextInputType.number,
+      //     inputFormatters: [
+      //       FilteringTextInputFormatter.digitsOnly,
+      //       NumericalRangeFormatter(min: 1, max: 256),
+      //     ],
+      //   );
+      // }),
+      _ConfigItem(
+          'setting.locale'.tr,
+          () => _getLocaleName(downloaderCfg.extra!.locale),
+          (Key key) => DropdownButton<String>(
+                key: key,
+                value: downloaderCfg.extra!.locale,
+                isDense: true,
+                onChanged: (value) async {
+                  downloaderCfg.extra!.locale = value!;
+                  controller.clearTapStatus();
+                  Get.updateLocale(toLocale(value));
 
-                      //   return TextField(
-                      //     controller: connectionsController,
-                      //     keyboardType: TextInputType.number,
-                      //     inputFormatters: [
-                      //       FilteringTextInputFormatter.digitsOnly,
-                      //       NumericalRangeFormatter(min: 1, max: 256),
-                      //     ],
-                      //   );
-                      // }),
-                      _ConfigItem(
-                          'setting.locale'.tr,
-                          () => _getLocaleName(setting.extra!.locale),
-                          () => DropdownButton<String>(
-                                value: setting.extra!.locale,
-                                isDense: true,
-                                onChanged: (value) async {
-                                  setting.extra!.locale = value!;
-                                  controller.clearTapStatus();
-                                  Get.updateLocale(toLocale(value));
+                  await debounceSave();
+                },
+                items: _getLocales()
+                    .map((e) => DropdownMenuItem<String>(
+                          value: e.substring(_settingLocaleKey.length),
+                          child: Text(e.tr),
+                        ))
+                    .toList(),
+              )),
+    ];
+    final basicKeys = basicConfigItems.map((e) => GlobalKey()).toList();
 
-                                  await debounceSave();
-                                },
-                                items: _getLocales()
-                                    .map((e) => DropdownMenuItem<String>(
-                                          value: e.substring(
-                                              _settingLocaleKey.length),
-                                          child: Text(e.tr),
-                                        ))
-                                    .toList(),
-                              )),
-                    ]),
-                  )).paddingAll(16)),
-              Container(
-                color: Colors.blue,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('setting.advanced'.tr),
-                  ],
-                ),
-              ),
-            ],
-          ),
+    final advancedConfigItems = [
+      _ConfigItem(
+        '通信协议',
+        () => startCfg.network,
+        (Key key) => Row(
+          children: [
+            RadioListTile<String>(
+              contentPadding: EdgeInsets.all(0),
+              title: const Text('TCP'),
+              value: "tcp",
+              groupValue: startCfg.network,
+              onChanged: (String? value) {
+                startCfg.network = value!;
+              },
+            ),
+            RadioListTile<String>(
+              contentPadding: EdgeInsets.all(0),
+              title: const Text('Unix Socket'),
+              value: "unix",
+              groupValue: startCfg.network,
+              onChanged: (String? value) {
+                startCfg.network = value!;
+              },
+            ),
+          ],
         ),
       ),
-    );
+    ];
+    final advancedKeys = advancedConfigItems.map((e) => GlobalKey()).toList();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('setting.title'.tr),
+          centerTitle: true,
+        ),
+        body: GestureDetector(
+          onTap: () {
+            controller.clearTapStatus();
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('setting.basic'.tr, style: Get.textTheme.titleLarge),
+                _padding,
+                Obx(() => Card(
+                        child: Column(
+                      children: _buildConfigItems(controller.basicTapStatues,
+                          basicKeys, basicConfigItems),
+                    ))),
+                _padding,
+                Text('setting.advanced'.tr, style: Get.textTheme.titleLarge),
+                _padding,
+                Obx(() => Card(
+                        child: Column(
+                      children: [
+                        ..._buildConfigItems(controller.advancedTapStatues,
+                            advancedKeys, advancedConfigItems),
+                        const Divider(),
+                        DefaultTabController(
+                          length: 2,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Container(
+                                child: TabBar(tabs: [
+                                  Tab(text: "HTTP"),
+                                  Tab(text: "BitTorrent"),
+                                ]),
+                              ),
+                              Container(
+                                //Add this to give height
+                                height: MediaQuery.of(context).size.height,
+                                child: TabBarView(children: [
+                                  Container(
+                                    child: Text("Home Body"),
+                                  ),
+                                  Container(
+                                    child: Text("Articles Body"),
+                                  ),
+                                ]),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ))),
+              ],
+            ).paddingAll(16),
+          ),
+        ));
   }
 
-  List<Widget> _buildConfigItems(List<_ConfigItem> buildItems) {
+  void _tapInputWidget(GlobalKey key) {
+    GestureDetector? detector;
+    void searchForGestureDetector(BuildContext? element) {
+      element?.visitChildElements((element) {
+        if (element.widget is GestureDetector) {
+          detector = element.widget as GestureDetector?;
+        } else {
+          searchForGestureDetector(element);
+        }
+      });
+    }
+
+    searchForGestureDetector(key.currentContext);
+
+    detector?.onTap?.call();
+  }
+
+  List<Widget> _buildConfigItems(Map<int, bool> tapStatues,
+      List<GlobalKey> keys, List<_ConfigItem> buildItems) {
     final result = <Widget>[];
     for (var i = 0; i < buildItems.length; i++) {
       final buildItem = buildItems[i];
       result.add(ListTile(
           title: Text(buildItem.label),
-          subtitle: controller.tapStatues[i] ?? false
-              ? buildItem.inputItem()
+          subtitle: tapStatues[i] ?? false
+              ? buildItem.inputItem(keys[i])
               : Text(buildItem.text()),
           onTap: () {
-            controller.tapStatues[i] = true;
+            tapStatues[i] = true;
             // set other false
-            for (var j = 0; j < controller.tapStatues.length; j++) {
+            for (var j = 0; j < tapStatues.length; j++) {
               if (j != i) {
-                controller.tapStatues[j] = false;
+                tapStatues[j] = false;
               }
             }
+            WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+              _tapInputWidget(keys[i]);
+            });
           }));
-      result.add(const Divider());
+      if (i != buildItems.length - 1) {
+        result.add(const Divider());
+      }
     }
     return result;
   }
@@ -219,7 +295,7 @@ class SettingView extends GetView<SettingController> {
 class _ConfigItem {
   late String label;
   late String Function() text;
-  late Widget Function() inputItem;
+  late Widget Function(Key key) inputItem;
 
   _ConfigItem(this.label, this.text, this.inputItem);
 }
