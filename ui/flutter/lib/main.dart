1aa72b049b48d98c63a8feb88ab9fc162bc40c33
liwei <liwei-8466@qq.com>
2023-11-14 15:32:43 +0800

    test

--- ui/flutter/lib/main.dart
+++ ui/flutter/lib/main.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:window_manager/window_manager.dart';
 
 import 'api/api.dart' as api;
