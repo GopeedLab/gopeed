d07cf83f4591f68e17b099084c2242ba7bc55cee
liwei <liwei-8466@qq.com>
2023-12-12 09:21:44 +0800

    fix: sort of completed tasks

--- ui/flutter/lib/app/modules/task/controllers/task_downloaded_controller.dart
+++ ui/flutter/lib/app/modules/task/controllers/task_downloaded_controller.dart
@@ -3,5 +3,6 @@ import 'package:gopeed/app/modules/task/controllers/task_list_controller.dart';
 import '../../../../api/model/task.dart';
 
 class TaskDownloadedController extends TaskListController {
-  TaskDownloadedController() : super([Status.done], SortDirection.asc);
+  TaskDownloadedController()
+      : super([Status.done], (a, b) => b.updatedAt.compareTo(a.updatedAt));
 }
