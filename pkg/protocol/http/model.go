74beea6044b2a1cb2957e812ef61a4b9552121e0
Levi <liwei-8466@qq.com>
2024-01-18 18:38:44 +0800

    feat: auto create torrent task for http download (#334)

--- pkg/protocol/http/model.go
+++ pkg/protocol/http/model.go
@@ -8,6 +8,8 @@ type ReqExtra struct {
 
 type OptsExtra struct {
 	Connections int `json:"connections"`
+	// AutoTorrent when task download complete, and it is a .torrent file, it will be auto create a new task for the torrent file
+	AutoTorrent bool `json:"autoTorrent"`
 }
 
 // Stats for download
