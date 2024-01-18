d38c4e6ce851b09a9a1214cb075c9b7090219613
liwei <liwei-8466@qq.com>
2024-01-18 14:52:51 +0800

    fix

--- ui/flutter/lib/util/window_util.dart
+++ ui/flutter/lib/util/window_util.dart
@@ -14,24 +14,18 @@ const String _windowStateIsMaximized = "window.isMaximized";
 const String _windowStateWidth = "window.width";
 const String _windowStateHeight = "window.height";
 
-Timer? _debounceTimer;
-
 /// Save window state to storage.
-/// Debounce to avoid too many save.
-void saveState({bool? isMaximized, double? width, double? height}) {
-  _debounceTimer?.cancel();
-  _debounceTimer = Timer(const Duration(milliseconds: 500), () async {
-    final prefs = await SharedPreferences.getInstance();
-    if (isMaximized != null) {
-      await prefs.setBool(_windowStateIsMaximized, isMaximized);
-    }
-    if (width != null) {
-      await prefs.setDouble(_windowStateWidth, width);
-    }
-    if (height != null) {
-      await prefs.setDouble(_windowStateHeight, height);
-    }
-  });
+void saveState({bool? isMaximized, double? width, double? height}) async {
+  final prefs = await SharedPreferences.getInstance();
+  if (isMaximized != null) {
+    await prefs.setBool(_windowStateIsMaximized, isMaximized);
+  }
+  if (width != null) {
+    await prefs.setDouble(_windowStateWidth, width);
+  }
+  if (height != null) {
+    await prefs.setDouble(_windowStateHeight, height);
+  }
 }
 
 /// Load window state from storage.
