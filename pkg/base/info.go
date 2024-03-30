c9692e76e5777ad4f0ed68e1695a42415886a4d3
liwei <liwei-8466@qq.com>
2024-03-30 11:10:34 +0800

    fix: build flags type

--- pkg/base/info.go
+++ pkg/base/info.go
@@ -2,7 +2,7 @@ package base
 
 // Version is the build version, set at build time, using `go build -ldflags "-X github.com/GopeedLab/gopeed/pkg/base.Version=1.0.0"`.
 var Version string
-var InDocker bool
+var InDocker string
 
 func init() {
 	if Version == "" {
