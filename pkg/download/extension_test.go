1aa2f6022f631e825188cc2b35fa5176f2036213
liwei <liwei-8466@qq.com>
2024-07-30 11:19:51 +0800

    fix: test

--- pkg/download/extension_test.go
+++ pkg/download/extension_test.go
@@ -238,7 +238,7 @@ func TestDownloader_Extension_OnError(t *testing.T) {
 		select {
 		case err = <-errCh:
 			break
-		case <-time.After(time.Second * 30):
+		case <-time.After(time.Second * 60):
 			err = errors.New("timeout")
 		}
 
