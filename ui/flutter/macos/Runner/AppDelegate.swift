9da137955bd5265bf24f4b7a9523a4bb8ae0e8ad
Levi <liwei2633@gmail.com>
2025-01-10 16:34:19 +0800

    macos support

--- ui/flutter/macos/Runner/AppDelegate.swift
+++ ui/flutter/macos/Runner/AppDelegate.swift
@@ -1,8 +1,22 @@
 import Cocoa
 import FlutterMacOS
+import app_links
 
 @main
 class AppDelegate: FlutterAppDelegate {
+  public override func application(_ application: NSApplication,
+                                  continue userActivity: NSUserActivity,
+                                  restorationHandler: @escaping ([any NSUserActivityRestoring]) -> Void) -> Bool {
+
+    guard let url = AppLinks.shared.getUniversalLink(userActivity) else {
+      return false
+    }
+    
+    AppLinks.shared.handleLink(link: url.absoluteString)
+    
+    return false // Returning true will stop the propagation to other packages
+  }
+
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return false
   }
