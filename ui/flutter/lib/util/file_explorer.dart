5d84445f8f968d60c2b429d120d2709b45b3c087
unifan <821869798@qq.com>
2025-06-07 11:14:58 +0800

    feat: Open in Folder with support for 3rd file manager (#1045)

--- ui/flutter/lib/util/file_explorer.dart
+++ ui/flutter/lib/util/file_explorer.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
-
+import 'package:open_dir/open_dir.dart';
+import 'package:path/path.dart' as path;
 import 'package:url_launcher/url_launcher_string.dart';
 
 class FileExplorer {
@@ -12,42 +13,19 @@ class FileExplorer {
   }
 
   static Future<void> _openDirectory(String directoryPath) async {
-    await launchUrlString("file://$directoryPath");
+    if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
+      await OpenDir().openNativeDir(path: directoryPath);
+    } else {
+      await launchUrlString("file://$directoryPath");
+    }
   }
 
   static Future<void> _openFile(String filePath) async {
-    if (Platform.isWindows) {
-      Process.run('explorer.exe', ['/select,', filePath]);
-    } else if (Platform.isMacOS) {
-      Process.run('open', ['-R', filePath]);
-    } else if (Platform.isLinux) {
-      _linuxOpen(filePath);
-    }
-  }
-
-  static Future<void> _linuxOpen(String filePath) async {
-    if (await Process.run('which', ['xdg-open'])
-        .then((value) => value.exitCode == 0)) {
-      final result = await Process.run('xdg-open', [filePath]);
-      if (result.exitCode != 0) {
-        _openWithFileManager(filePath);
-      }
-    } else {
-      _openWithFileManager(filePath);
-    }
-  }
-
-  static Future<void> _openWithFileManager(String filePath) async {
-    final desktop = Platform.environment['XDG_CURRENT_DESKTOP'];
-    if (desktop == null) {
-      throw Exception('XDG_CURRENT_DESKTOP is not set');
-    }
-    if (desktop == 'GNOME') {
-      await Process.run('nautilus', ['--select', filePath]);
-    } else if (desktop == 'KDE') {
-      await Process.run('dolphin', ['--select', filePath]);
-    } else {
-      throw Exception('Unsupported desktop environment');
+    if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
+      final fileName = path.basename(filePath);
+      final parentPath = path.dirname(filePath);
+      await OpenDir()
+          .openNativeDir(path: parentPath, highlightedFileName: fileName);
     }
   }
 }
