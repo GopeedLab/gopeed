3184fff02bbb172b9d2ee6291ac15f3cd6ec432d
Levi <liwei2633@gmail.com>
2026-01-08 10:43:43 +0800

    update

--- ui/flutter/lib/main.dart
+++ ui/flutter/lib/main.dart
@@ -121,10 +121,7 @@ Future<void> init(Args args) async {
   try {
     await controller.loadStartConfig();
     final startCfg = controller.startConfig.value;
-    // controller.runningPort.value = await LibgopeedBoot.instance.start(startCfg);
-    startCfg.network = "tcp";
-    startCfg.address = "127.0.0.1:9999";
-    controller.runningPort.value = 9999;
+    controller.runningPort.value = await LibgopeedBoot.instance.start(startCfg);
     api.init(startCfg.network, controller.runningAddress(), startCfg.apiToken);
   } catch (e) {
     logger.e("libgopeed init fail", e);
