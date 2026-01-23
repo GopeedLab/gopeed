b9fb1ea63db3704439cede141deec873860095e0
Levi <liwei2633@gmail.com>
2026-01-23 17:03:23 +0800

    update

--- ui/flutter/macos/Runner/AppDelegate.swift
+++ ui/flutter/macos/Runner/AppDelegate.swift
@@ -21,6 +21,10 @@ class AppDelegate: FlutterAppDelegate {
     return false
   }
 
+  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+
   override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
     if !flag {
       for window in NSApp.windows {
