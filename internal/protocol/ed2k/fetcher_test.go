9e7f0bfa8a785063c25a93f5ed39988fbe6ff257
Levi <liwei2633@gmail.com>
2026-03-17 17:58:12 +0800

    update test case

--- internal/protocol/ed2k/fetcher_test.go
+++ internal/protocol/ed2k/fetcher_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/GopeedLab/gopeed/pkg/base"
 )
 
-const testLink = "ed2k://|file|Taylor,%20Elizabeth%20-%20Prohibido%20morir%20aqui%20[66672]%20(r1.0).epub|434885|23A8CEFF57A7A32D562D649ED7893796|/"
+const testLink = "ed2k://|file|cn_windows_10_multi-edition_vl_version_1709_updated_sept_2017_x64_dvd_100090774.iso|4630972416|8867C5E54405FF9452225B66EFEE690A|/"
 
 func TestFetcher_Resolve(t *testing.T) {
 	f := (&FetcherManager{}).Build()
@@ -22,20 +22,20 @@ func TestFetcher_Resolve(t *testing.T) {
 	if meta.Res == nil {
 		t.Fatal("Resolve() resource is nil")
 	}
-	if got, want := meta.Res.Hash, "23A8CEFF57A7A32D562D649ED7893796"; got != want {
+	if got, want := meta.Res.Hash, "8867C5E54405FF9452225B66EFEE690A"; got != want {
 		t.Fatalf("Resolve() hash = %s, want %s", got, want)
 	}
-	if got, want := meta.Res.Size, int64(434885); got != want {
+	if got, want := meta.Res.Size, int64(4630972416); got != want {
 		t.Fatalf("Resolve() size = %d, want %d", got, want)
 	}
-	if got, want := meta.Res.Files[0].Name, "Taylor, Elizabeth - Prohibido morir aqui [66672] (r1.0).epub"; got != want {
+	if got, want := meta.Res.Files[0].Name, "cn_windows_10_multi-edition_vl_version_1709_updated_sept_2017_x64_dvd_100090774.iso"; got != want {
 		t.Fatalf("Resolve() name = %q, want %q", got, want)
 	}
 }
 
 func TestFetcherManager_ParseName(t *testing.T) {
 	got := (&FetcherManager{}).ParseName(testLink)
-	want := "Taylor, Elizabeth - Prohibido morir aqui [66672] (r1.0).epub"
+	want := "cn_windows_10_multi-edition_vl_version_1709_updated_sept_2017_x64_dvd_100090774.iso"
 	if got != want {
 		t.Fatalf("ParseName() = %q, want %q", got, want)
 	}
