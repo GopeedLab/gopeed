0efc85d3c077aa8d7f6f8a020430c6e967d79024
Levi <liwei-8466@qq.com>
2026-09-08 23:47:38 +0800

    feat: unify native API invocation with async callbacks (#1476)

--- ui/flutter/ios/Runner/GopeedTaskEventForwarder.m
+++ ui/flutter/ios/Runner/GopeedTaskEventForwarder.m
@@ -8,6 +8,45 @@
 
 @end
 
+@interface GopeedInvokeResultForwarder : NSObject <LibgopeedInvokeResultListener>
+
+@property(nonatomic, copy, nullable) FlutterResult result;
+
+- (instancetype)initWithResult:(FlutterResult)result;
+
+@end
+
+@implementation GopeedInvokeResultForwarder
+
+- (instancetype)initWithResult:(FlutterResult)result {
+  self = [super init];
+  if (self) {
+    _result = [result copy];
+  }
+  return self;
+}
+
+- (void)onResult:(int64_t)requestID
+         success:(BOOL)success
+         payload:(NSString * _Nullable)payload {
+  FlutterResult result = self.result;
+  self.result = nil;
+  if (result == nil) {
+    return;
+  }
+  dispatch_async(dispatch_get_main_queue(), ^{
+    if (success) {
+      result(payload ?: @"");
+    } else {
+      result([FlutterError errorWithCode:@"ERROR"
+                                 message:payload ?: @"InvokeAsync failed"
+                                 details:nil]);
+    }
+  });
+}
+
+@end
+
 @implementation GopeedTaskEventForwarder
 
 - (instancetype)initWithChannel:(FlutterMethodChannel *)channel {
@@ -32,4 +71,16 @@ void GopeedSubscribeTaskEventsWithForwarder(
     int64_t mask,
     GopeedTaskEventForwarder * _Nullable listener) {
   LibgopeedSubscribeTaskEvents(mask, listener);
+}
+
+void GopeedInvokeAsyncWithResult(
+    NSString *method,
+    NSString *path,
+    NSString *query,
+    NSString *body,
+    int64_t requestID,
+    FlutterResult result) {
+  GopeedInvokeResultForwarder *forwarder =
+      [[GopeedInvokeResultForwarder alloc] initWithResult:result];
+  LibgopeedInvokeAsync(method, path, query, body, requestID, forwarder);
 }
