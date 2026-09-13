62e8ab3cefcb6e2c231e76f1b42628723307570c
Levi <liwei2633@gmail.com>
2026-09-13 11:57:26 +0800

    fix(ui): show downloaded bytes for tasks with unknown size (#1508)

--- ui/flutter/lib/features/tasks/presentation/widgets/task_card/task_card_parts.dart
+++ ui/flutter/lib/features/tasks/presentation/widgets/task_card/task_card_parts.dart
@@ -95,7 +95,11 @@ class TaskCardFooter extends StatelessWidget {
         children: [
           Expanded(
             child: Text(
-              task.total != null ? '${task.downloaded} / ${task.total}' : context.l10n.unknownSize,
+              task.total != null
+                  ? '${task.downloaded} / ${task.total}'
+                  : task.downloadedBytes == 0
+                  ? context.l10n.unknownSize
+                  : task.downloaded,
               maxLines: 1,
               overflow: TextOverflow.ellipsis,
               style: TextStyle(color: leftColor, fontSize: 10, height: 1),
